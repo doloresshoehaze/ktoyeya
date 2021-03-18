da84cc9b076c3fd5f99ab64b8f20394645cb2277
Dolores <koliada.mv@gmail.com>
2021-03-18 16:49:46 +0300

    added dictionary

---
+++ dictionary.py
@@ -0,0 +1,102 @@
+meaning1 = 'l"éléments d"un ensemble / элемент общности, деталь'
+meaning2 = 'morceau de métal façonné servant de monnaie / кусок обработанного металла, служащий валютой, монета'
+meaning3 = 'œuvre dramatique / драматическое произведение'
+meaning4 = 'la chambre / комната'
+
+
+dictionary = {
+    meaning1: {
+        'attachée': {
+            'pos': -3,
+        },
+        'défectueuse': {
+            'pos': 1,
+        },
+        'puzzle': {
+            'pos': 2,
+        },
+        'plastique': {
+            'pos': 2,
+        },
+        'machine': {
+            'pos': 3,
+        },
+        'colle': {
+            'pos': 3,
+        },
+        'complexe': {
+            'pos': 4,
+        }
+    },
+    meaning2: {
+        'valeur': {
+            'pos': -4,
+
+        },
+        'fouilles': {
+            'pos': -2,
+        },
+
+        'rare': {
+            'pos': 1,
+        },
+        'argent': {
+            'pos': 2,
+        },
+        'frappée': {
+            'pos': 3,
+        },
+        'époques': {
+            'pos': 3,
+        },
+        'dénominations': {
+            'pos': 5,
+        }
+    },
+    meaning3: {
+        'jouer': {
+            'pos': -2,
+        },
+        'écrire': {
+            'pos': -2,
+        },
+        'nouvelle': {
+            'pos': -1,
+        },
+        'vocales': {
+            'pos': 1,
+        },
+        'propos': {
+            'pos': 2,
+        },
+        'fantastique': {
+            'pos': 1,
+        },
+        'théatre': {
+            'pos': 2,
+        }
+    },
+    meaning4: {
+        'belle': {
+            'pos': -1,
+        },
+        'petite': {
+            'pos': -1,
+        },
+        'vitrée': {
+            'pos': 1,
+        },
+        'nettoyer': {
+            'pos': 2,
+        },
+        'maison': {
+            'pos': 3,
+        },
+        'garage': {
+            'pos': 3,
+        },
+        'prendre': {
+            'pos': 4,
+        }
+    }
+}
